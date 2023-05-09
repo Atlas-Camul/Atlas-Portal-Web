3496d004cd225c659788fe64516e7d618ad0e21d
Tiago Machado <1191097@isep.ipp.pt>
2023-05-10 00:58:36 +0100

    Logout

--- src/components/DropdownUser.jsx
+++ src/components/DropdownUser.jsx
@@ -152,7 +152,7 @@ const DropdownUser = () => {
             </Link>
           </li>
         </ul> */}
-        <button className='flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base'>
+        <Link to='/auth/signup' className='flex items-center gap-3.5 py-4 px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base'>
           <svg
             className='fill-current'
             width='22'
@@ -171,7 +171,7 @@ const DropdownUser = () => {
             />
           </svg>
           Log Out
-        </button>
+        </Link>
       </div>
       {/* <!-- Dropdown End --> */}
     </div>
