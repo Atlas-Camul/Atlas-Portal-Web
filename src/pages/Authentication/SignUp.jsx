207d2105ff3cb822a6114cf9091de3bc7bf37fe7
Mayron-Martins <mayronmartins013@gmail.com>
2023-05-31 23:37:26 +0100

    Init

--- src/pages/Authentication/SignUp.jsx
+++ src/pages/Authentication/SignUp.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import LogoExtended from '../../images/logo/logo-extended.png';
 import { Link } from 'react-router-dom';
--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -4,10 +4,9 @@ import Breadcrumb from '../components/Breadcrumb';
 import TableThree from '../components/TableThree';
 
 
-const SignUp = () => {
+
     function submitClient() {
         const element = inputElements();
-
 
         fetch('/user_management', {
             method: 'GET',
@@ -27,8 +26,8 @@ const SignUp = () => {
                 console.error('Erro na requisição POST 1:', error);
             });
     }
-    function inputElements() {
 
+    function inputElements() {
         const name = document.getElementById('fullName').value;
         const email = document.getElementById('emailUser').value;
 
@@ -45,69 +44,69 @@ const SignUp = () => {
         return element;
     }
 
-const Tables = () => {
-  return (
-    <DefaultLayout>
-      <Breadcrumb pageName='User Management' />
-      <div className='flex flex-col gap-10'>
-        <div className='col-span-5 xl:col-span-3'>
-          <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
-            <div className='border-b border-stroke py-4 px-7 dark:border-strokedark'>
-              <h3 className='font-medium text-black dark:text-white'>
-                Search user
-              </h3>
-            </div>
-            <div className='p-7'>
-              <form action='#'>
-                <div className='mb-5.5 flex flex-col gap-5.5 sm:flex-row'>
-                  <div className='w-full sm:w-1/2'>
-                    <label
-                      className='mb-3 block text-sm font-medium text-black dark:text-white'
-                      htmlFor='fullName'
-                    >
-                      Name
-                    </label>
-                    <div className='relative'>
-                      <input
-                        className='w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
-                        type='text'
-                        name='fullName'
-                        id='fullName'
-                      />
-                    </div>
-                  </div>
+    const Tables = () => {
+        return (
+            <DefaultLayout>
+                <Breadcrumb pageName='User Management' />
+                <div className='flex flex-col gap-10'>
+                    <div className='col-span-5 xl:col-span-3'>
+                        <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
+                            <div className='border-b border-stroke py-4 px-7 dark:border-strokedark'>
+                                <h3 className='font-medium text-black dark:text-white'>
+                                    Search user
+                                </h3>
+                            </div>
+                            <div className='p-7'>
+                                <form action='#'>
+                                    <div className='mb-5.5 flex flex-col gap-5.5 sm:flex-row'>
+                                        <div className='w-full sm:w-1/2'>
+                                            <label
+                                                className='mb-3 block text-sm font-medium text-black dark:text-white'
+                                                htmlFor='fullName'
+                                            >
+                                                Name
+                                            </label>
+                                            <div className='relative'>
+                                                <input
+                                                    className='w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
+                                                    type='text'
+                                                    name='fullName'
+                                                    id='fullName'
+                                                />
+                                            </div>
+                                        </div>
 
-                  <div className='w-full sm:w-1/2'>
-                    <label
-                      className='mb-3 block text-sm font-medium text-black dark:text-white'
-                      htmlFor='phoneNumber'
-                    >
-                      Email
-                    </label>
-                    <input
-                      className='w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
-                      type='text'
-                      name='emailUser'
-                      id='emailUser'
-                    />
-                  </div>
+                                        <div className='w-full sm:w-1/2'>
+                                            <label
+                                                className='mb-3 block text-sm font-medium text-black dark:text-white'
+                                                htmlFor='phoneNumber'
+                                            >
+                                                Email
+                                            </label>
+                                            <input
+                                                className='w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
+                                                type='text'
+                                                name='emailUser'
+                                                id='emailUser'
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='flex justify-end gap-4.5'>
+                                        <button
+                                            className='flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:shadow-1'
+                                            type='submit'
+                                        >
+                                            Search user
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <TableThree />
                 </div>
-                <div className='flex justify-end gap-4.5'>
-                  <button
-                    className='flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:shadow-1'
-                    type='submit'
-                  >
-                    Search user
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <TableThree />
-      </div>
-    </DefaultLayout>
-  )
+            </DefaultLayout>
+        )
 }
 
 export default Tables;
