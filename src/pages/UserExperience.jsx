0dda39bd9667f1e451231a56c2be454d54b4760a
WendersonBastos <wrbastos.bastos@gmail.com>
2023-06-03 16:16:38 +0100

    userexperience feito

--- src/pages/UserExperience.jsx
+++ src/pages/UserExperience.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import DefaultLayout from '../layout/DefaultLayout';
 import Breadcrumb from '../components/Breadcrumb';
 import TableTwo from '../components/TableTwo';
 
 const Tables = () => {
+        const [jsonData, setJsonData] = useState([]);
+    //List All Users
+    useEffect(() => {
+        fetch('/user-experience/list')
+            .then(res => res.json())
+            .then(data => {
+                if ('status' in data) {
+                    return;
+                }
+
+                if (Array.isArray(data)) {
+                    setJsonData(data);
+                } else {
+                    setJsonData([data]);
+                }
+               
+            });
+    }, []);
+
+
+
+
+
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName='User Experience' />
       <div className='flex flex-col gap-10'>
-        <TableTwo />
+        <TableTwo jsonData = {jsonData} />
       </div>
     </DefaultLayout>
   )
